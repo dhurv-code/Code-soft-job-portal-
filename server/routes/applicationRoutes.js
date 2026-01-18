240556f60573eb61a5f4fad410c28bad681a937a
Dhuruv gupta #1 <160989356+dhurv-code@users.noreply.github.com>
2026-01-18 13:23:14 +0530

    Update applicationRoutes.js

--- server/routes/applicationRoutes.js
+++ server/routes/applicationRoutes.js
@@ -7,10 +7,8 @@ import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-// jobseeker apply
 router.post("/:jobId", protect, applyToJob);
 
-// employer view applicants
 router.get("/job/:jobId", protect, getApplicantsForJob);
 
 export default router;
